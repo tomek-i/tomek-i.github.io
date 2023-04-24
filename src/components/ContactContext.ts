e667575ba0627e252cb547f4456f48dab59dafce
tomek-i <Tomek.iwainski@gmail.com>
2023-04-24 18:19:10 +1000

    Update ContactContext.ts

--- src/components/ContactContext.ts
+++ src/components/ContactContext.ts
@@ -1,13 +1,11 @@
-import { createContext, useContext } from "react";
-import { JobCardContext } from "./JobCard/useCardContext";
-
-export const ContactContext = createContext(null);
+import { useContext } from 'react';
+import { JobCardContext } from './JobCard/useCardContext';
 
 export function useJobCardContext() {
   const context = useContext(JobCardContext);
   if (!context)
     throw new Error(
-      "JobCard.* component must be rendered as child of JobCard component"
+      'JobCard.* component must be rendered as child of JobCard component'
     );
 
   return context;
