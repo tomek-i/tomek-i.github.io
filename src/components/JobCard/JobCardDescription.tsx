dd5f58a689b88ce7deb12cdff46e7ce2ae704e1b
tomek-i <Tomek.iwainski@gmail.com>
2023-04-24 18:19:14 +1000

    Update JobCardDescription.tsx

--- src/components/JobCard/JobCardDescription.tsx
+++ src/components/JobCard/JobCardDescription.tsx
@@ -1,12 +1,12 @@
-import { useJobCardContext } from "./useCardContext";
+import { useJobCardContext } from './useCardContext';
 
 interface JobCardDescriptionProps {}
 export const JobCardDescription: React.FC<JobCardDescriptionProps> = () => {
   const { job } = useJobCardContext();
   return (
     <>
-      {job.description && (
-        <p className="text-left job-card_description">{job.description}</p>
+      {job.summary && (
+        <p className="text-left job-card_description">{job.summary}</p>
       )}
     </>
   );
