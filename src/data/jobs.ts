d7f2777c2ca4ab9ad123bcf4eb594f8c071a57ae
tomek-i <Tomek.iwainski@gmail.com>
2023-04-24 18:19:19 +1000

    Update jobs.ts

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -7,8 +7,21 @@ export const jobs: Timeline[] = [
       website: 'https://www.4mation.com.au',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis mollitia quisquam earum temporibus fugit natus! Nemo libero exercitationem quisquam nam doloremque minus fugit, ipsa temporibus nostrum alias laudantium blanditiis cupiditate?',
+      address: {
+        city: '',
+        country: '',
+        postcode: '',
+        state: '',
+        street: '',
+      },
+      loctation: {
+        lat: 0,
+        long: 0,
+      },
     },
     job: {
+      responsibilities: [],
+      role: '',
       summary: '',
       image:
         'https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/timeline/images/img-13.png',
