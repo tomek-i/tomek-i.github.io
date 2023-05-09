84a383926c7b44547e77c0b1ec49a5a161bba1b2
tomek-i <Tomek.iwainski@gmail.com>
2023-05-09 12:37:42 +1000

    code stats service

--- src/types/index.ts
+++ src/types/index.ts
@@ -48,3 +48,32 @@ export interface Post {
   //TODO: rename `attributes` to meta and use Post<T> to pecify the type of the meta information
   attributes: Meta;
 }
+
+//TODO: refactor this out to a separate file specific to code::stats
+export type Experience = number;
+export interface CodeStatsDates {
+  [date: string]: Experience;
+}
+
+//TODO: the new_xps and xps can be refactored out
+export interface CodeStatsLanguageExperience {
+  [language: string]: {
+    new_xps: number;
+    xps: number;
+  };
+}
+export interface CodeStatsMachines {
+  [date: string]: {
+    new_xps: number;
+    xps: number;
+  };
+}
+
+export interface CodeStatsResponse {
+  dates: Map<string, number>; //OR :CodeStatsDates
+  languages: CodeStatsLanguageExperience;
+  machines: CodeStatsMachines;
+  new_xp: number;
+  total_xp: number;
+  user: string;
+}
