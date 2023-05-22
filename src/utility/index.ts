9327e839b2524632e342e546766c582cdcc8194c
tomek-i <Tomek.iwainski@gmail.com>
2023-05-22 21:16:07 +1000

    add format date utility function

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,5 +1,14 @@
 export const sort = (a: unknown, b: unknown, direction: 'asc' | 'desc') => {
+  // console.log('SORT', { a, b, an: Number(a), bn: Number(b) });
   return direction === 'asc' ? Number(a) - Number(b) : Number(b) - Number(a);
 };
 export const asc = (a: unknown, b: unknown) => sort(a, b, 'asc');
 export const desc = (a: unknown, b: unknown) => sort(a, b, 'desc');
+
+export const formatDate = (date: Date) => {
+  // Get month and year
+  const month = date.getMonth() + 1; // Months are zero-based
+  const year = date.getFullYear();
+  // Format the date as "mm/yyyy"
+  return `${month.toString().padStart(2, '0')}/${year}`;
+};
