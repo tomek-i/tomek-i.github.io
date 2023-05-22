4272ef6c5a5995e9aa1ba8fd1761d93a973b48c5
tomek-i <Tomek.iwainski@gmail.com>
2023-05-22 21:17:10 +1000

    Update usePosts.ts

--- src/components/hooks/usePosts.ts
+++ src/components/hooks/usePosts.ts
@@ -8,6 +8,18 @@ class TagCount {
   constructor(public tag: string, public count: number) {}
 }
 
+function parseDateInAustralianFormat(dateString: string): Date {
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+  };
+  const formattedDateString = new Intl.DateTimeFormat('en-AU', options).format(
+    new Date(dateString)
+  );
+  return new Date(Date.parse(formattedDateString));
+}
+
 function countTags(posts: PostType[]): TagCount[] {
   const tagCounts = new Map<string, number>();
   for (const obj of posts) {
@@ -45,13 +57,39 @@ export const usePosts = () => {
             .then((res) => res.text())
             .then((content) => {
               const attributes = fm(content).attributes as Meta;
+
+              //TODO: is there a better way, the frontmatter reads it as string and we need to change it back to dates??
+
+              // Create an object with the desired format options
+              const options: Intl.DateTimeFormatOptions = {
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric',
+              };
+
+              if (typeof attributes.job.dates.start === 'string') {
+                attributes.job.dates.start = parseDateInAustralianFormat(
+                  attributes.job.dates.start
+                );
+              }
+
+              // Parse the end date if it's a string
+              if (typeof attributes.job.dates.end === 'string') {
+                attributes.job.dates.end = parseDateInAustralianFormat(
+                  attributes.job.dates.end
+                );
+              }
               return {
                 content,
                 attributes,
               };
             });
         })
-      ).then((results) => results.sort(desc));
+      ).then((results) =>
+        results.sort((a, b) => {
+          return desc(a.attributes.job.dates.start, b.attributes.job.dates.end);
+        })
+      );
 
       setIsLoading(false);
       setPosts(results);
