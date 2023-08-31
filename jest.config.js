3e80c09c3e62f2f185dcd02a2f40c966308dd4a8
tomek-i <tomek.iwainski@gmail.com>
2023-08-31 17:14:40 +1000

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 // jest.config.js
 
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/','/scripts/.*\\.js$'], // Ignore the node_modules folder
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.ts',
+    '!src/**/index.ts',
+    '!src/stories/**/*.tsx',
+  ],
+  setupFiles: ['react-app-polyfill/jsdom'],
+  resetMocks: true,
+  testPathIgnorePatterns: ['/node_modules/', '/scripts/.*\\.js$'], // Ignore the node_modules folder
 };
