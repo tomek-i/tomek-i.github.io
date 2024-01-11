82f3590430c81a8136fbd5699e1ebe7e0dca6bf2
tomek-i <tomek.iwainski@gmail.com>
2024-01-11 15:44:05 +1100

    Add specific configuration for production development and eslint-disable-next-line import/no-anonymous-default-export

--- src/configuration/configs/config.prod.ts
+++ src/configuration/configs/config.prod.ts
@@ -1,2 +1,3 @@
 //TODO: add specific configuration for production development and or fetch from .env / github for secrets
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {};
