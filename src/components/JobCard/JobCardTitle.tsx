cb00c4c4a8a0e74d98c2ab01f6bde91b0393b020
tomek-i <tomek.iwainski@gmail.com>
2024-01-11 17:03:46 +1100

    Update JobCardTitle component to display company name in uppercase

--- src/components/JobCard/JobCardTitle.tsx
+++ src/components/JobCard/JobCardTitle.tsx
@@ -3,5 +3,7 @@ import { useJobCardContext } from './useCardContext';
 interface JobCardTitleProps {}
 export const JobCardTitle: React.FC<JobCardTitleProps> = () => {
   const { company } = useJobCardContext();
-  return <div className="text-left job-card_title">{company.name}</div>;
+  return (
+    <div className="text-left uppercase job-card_title">{company.name}</div>
+  );
 };
