2069478966f27fb8578e7ca27bd041f3f5dc5f39
tomek-i <tomek.iwainski@gmail.com>
2024-01-12 01:24:05 +1100

    Add getAllPosts method to PostService

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -2,13 +2,29 @@ import fm from 'front-matter';
 
 import { Frontmatter, Post } from '../types';
 
+/**
+ * Imports all modules matching the given webpack context.
+ * @param r - The webpack context.
+ * @returns An array of imported modules.
+ */
 const importAll = (r: any) => r.keys().map(r);
+
+
 //NOTE: the following code does not support folders which makes it hard to organise content eg. work vs projects.
 //      Need to find some solutions to load projects and or abe to separate them somehow
+/**
+ * Array of imported markdown files.
+ * @type {Array<unknown>}
+ */
 const markdownFiles = importAll(
   (require as any).context(process.env.REACT_APP_CONTENT_PATH, false, /\.md$/),
 );
 
+
+/**
+ * Retrieves all posts asynchronously.
+ * @returns A promise that resolves to an array of Post objects with Frontmatter.
+ */
 const getAllPosts = async () => {
   return Promise.all<Post<Frontmatter>[]>(
     markdownFiles.map(
@@ -26,6 +42,14 @@ const getAllPosts = async () => {
   );
 };
 
+
+/**
+ * Service for posts.
+ */
 export const PostService = {
+  /**
+   * Retrieves all posts.
+   * @returns {Array} An array of posts.
+   */
   getAllPosts,
 };
