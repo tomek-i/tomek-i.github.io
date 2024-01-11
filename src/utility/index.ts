b9ca0fc9a0ac88e34a6e3c1452e93cfab52cd8ce
tomek-i <tomek.iwainski@gmail.com>
2024-01-12 01:23:55 +1100

    Add utility functions for date calculations and sorting

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,5 +1,11 @@
 import { Frontmatter, Post } from '../types';
 
+/**
+ * Calculates the total number of years between a start date and an end date.
+ * @param startDate The start date.
+ * @param endDate The end date.
+ * @returns The total number of years between the start and end dates.
+ */
 export function calculateTotalYears(startDate: Date, endDate: Date) {
   const startYear = startDate.getFullYear();
 
@@ -7,30 +13,43 @@ export function calculateTotalYears(startDate: Date, endDate: Date) {
 
   let totalYears = endYear - startYear;
 
-  // // Check if the end date's month and day are earlier than the start date's month and day
-  // if (startMonth > endMonth || (startMonth === endMonth && startDay > endDay)) {
-  //   totalYears--;
-  // }
-
-  // // Check if the end date's month and day are later than the start date's month and day
-  // if (
-  //   startMonth < endMonth ||
-  //   (startMonth === endMonth && startDay <= endDay)
-  // ) {
-  //   totalYears++;
-  // }
-
   return totalYears;
 }
 
+/**
+ * Sorts two values in ascending or descending order.
+ * @param a The first value.
+ * @param b The second value.
+ * @param direction The sort direction, either 'asc' for ascending or 'desc' for descending.
+ * @returns The sorted values.
+ */
 export const sort = (a: unknown, b: unknown, direction: 'asc' | 'desc') => {
   return direction === 'asc'
     ? (a as any) - (b as any)
     : (b as any) - (a as any);
 };
+
+/**
+ * Sorts two values in ascending order.
+ * @param a The first value.
+ * @param b The second value.
+ * @returns The sorted values in ascending order.
+ */
 export const asc = (a: unknown, b: unknown) => sort(a, b, 'asc');
+
+/**
+ * Sorts two values in descending order.
+ * @param a The first value.
+ * @param b The second value.
+ * @returns The sorted values in descending order.
+ */
 export const desc = (a: unknown, b: unknown) => sort(a, b, 'desc');
 
+/**
+ * Formats a date as "mm/yyyy".
+ * @param date The date to format.
+ * @returns The formatted date string.
+ */
 export const formatDate = (date: Date) => {
   // Get month and year
   const month = date.getMonth() + 1; // Months are zero-based
@@ -39,16 +58,34 @@ export const formatDate = (date: Date) => {
   return `${month.toString().padStart(2, '0')}/${year}`;
 };
 
+/**
+ * Represents a tag and its count.
+ */
 export class TagCount {
+  /**
+   * Creates a new instance of TagCount.
+   * @param tag The tag name.
+   * @param count The count of the tag.
+   */
   constructor(public tag: string, public count: number) {}
 }
 
+/**
+ * Parses a date string in Australian format (dd/mm/yyyy) and returns a Date object.
+ * @param dateString The date string in Australian format.
+ * @returns The parsed Date object.
+ */
 export function parseDateInAustralianFormat(dateString: string): Date {
   const [day, month, year] = dateString.split('/');
   // JavaScript Date expects month values in the range of 0-11, so subtract 1 from the parsed month
   return new Date(Number(year), Number(month) - 1, Number(day));
 }
 
+/**
+ * Counts the occurrences of each tag in an array of posts.
+ * @param posts The array of posts.
+ * @returns An array of TagCount objects representing the tag counts.
+ */
 export function countTags(posts: Post<Frontmatter>[]): TagCount[] {
   const tagCounts = new Map<string, number>();
   for (const obj of posts) {
