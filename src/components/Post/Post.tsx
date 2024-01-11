62b79702d34105cb910a3a1de9135b2d3dbc23f1
tomek-i <tomek.iwainski@gmail.com>
2024-01-11 17:07:20 +1100

    Add company website link and display company name in uppercase

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom';
 import frontmatter from 'remark-frontmatter';
 
+import { config } from '../../configuration';
+import { EnvironmentTypes } from '../../configuration/EnvironmentTypes';
 import { Frontmatter, Post as PostType } from '../../types';
 
 interface PostProps {
@@ -11,23 +13,30 @@ interface PostProps {
 export const Post: React.FC<PostProps> = ({ post }) => {
   //TODO: incorporate and show some of the information stored in the attributes / meta
   return (
-    <div className="container flex flex-col mx-auto">
+    <div className="container flex flex-col mx-auto cursor-default">
       <div className="text-center">
         {/* TODO: add company LOGO */}
-        <h1>{post.frontmatter.company.name}</h1>
+        <h1 className="uppercase ">{post.frontmatter.company.name}</h1>
         <Link
           to={post.frontmatter.company.website}
           target="_blank"
           rel="nofollow"
         >
           <button className="m-0 text-xs">
-            {post.frontmatter.company.website}
+            {post.frontmatter.company.website
+              ?.replace('https://', '')
+              ?.replace('http://', '')}
           </button>
         </Link>
       </div>
-      {JSON.stringify(post.frontmatter)}
       {/* TODO: add some pictures from workplace ? */}
       <ReactMarkdown children={post.content} remarkPlugins={[frontmatter]} />
+
+      {config.environment === EnvironmentTypes.LOCAL && (
+        <pre>
+          <code>{JSON.stringify(post.frontmatter, null, 2)}</code>
+        </pre>
+      )}
     </div>
   );
 };
