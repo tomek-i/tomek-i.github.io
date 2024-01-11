e0e28c46449e1370f8a1dd1b4af656acf8fea2a4
tomek-i <tomek.iwainski@gmail.com>
2024-01-12 01:23:37 +1100

    Refactor home page component and add timeline functionality

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,69 +8,68 @@ import { usePosts } from '../components/hooks/usePosts';
 // import { jobs } from '../data/jobs';
 
 interface HomePageProps {}
+
 export const HomePage: React.FC<HomePageProps> = () => {
-  const { posts } = usePosts();
   //TODO: see App.tsx, we need the posts there too and should refactor
+  const { posts } = usePosts();
   return (
     <>
-    <HelmetProvider >
-      {/* //TODO: helmet is outdated https://stackoverflow.com/questions/66045965/warning-using-unsafe-componentwillmount-in-strict-mode-is-not-recommended-upgr */}
-      <Helmet>
-        <script defer async src="./timeline.js" type="text/javascript"></script>
-      </Helmet>
+      <HelmetProvider>
+        <Helmet>
+          <script
+            defer
+            async
+            src="./timeline.js"
+            type="text/javascript"
+          ></script>
+        </Helmet>
 
-      {/* //TODO: make compaund component */}
+        {/* //TODO: make compound component */}
 
-      <AboutMe />
+        <AboutMe />
 
-      <Section>
-        <Section.Wrapper>
-          <Section.Title title="Timeline" alignment="center" />
-          <p className="text-center">
-            Below you can find my career path in form of a timeline. Click on
-            the Details button for more information about each job and position.
-          </p>
-        </Section.Wrapper>
-      </Section>
+        <Section>
+          <Section.Wrapper>
+            <Section.Title title="Timeline" alignment="center" />
+            <p className="text-center">
+              Below you can find my career path in form of a timeline. Click on
+              the Details button for more information about each job and
+              position.
+            </p>
+          </Section.Wrapper>
+        </Section>
 
-      {/* TODO: separate this out as it can be a separate page called career where this component will be reused */}
-      <Section className="overflow-hidden">
-        <div className="timeline-format-container">
-          <div className="js-timeline timeline">
-            <div className="js-timeline_line timelime_line">
-              <div className="js-timeline_line-progress timelime_line-progress"></div>
-            </div>
-            <div className="_list">
-              {posts?.map((post, index) => (
-                <TimelineItem
-                  timeline={post.frontmatter}
-                  isAlternate={index % 2 !== 0}
-                  key={post.frontmatter.company.name + 2 + index}
-                />
-              ))}
-              {/* {jobs.map((job, index) => (
-                <TimelineItem
-                  timeline={job}
-                  isAlternate={index % 2 !== 0}
-                  key={job.company.name + index}
-                />
-              ))} */}
+        {/* TODO: separate this out as it can be a separate page called career where this component will be reused */}
+        <Section className="overflow-hidden">
+          <div className="timeline-format-container">
+            <div className="js-timeline timeline">
+              <div className="js-timeline_line timelime_line">
+                <div className="js-timeline_line-progress timelime_line-progress"></div>
+              </div>
+              <div className="_list">
+                {posts?.map((post, index) => (
+                  <TimelineItem
+                    timeline={post.frontmatter}
+                    isAlternate={index % 2 !== 0}
+                    key={post.frontmatter.company.name + 2 + index}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </Section>
+        </Section>
 
-      <Section className="spikes">
-        <Section.Wrapper>
-          <Section.Title title="section 3" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum neque
-            velit saepe hic blanditiis maxime ipsam assumenda consectetur,
-            voluptatibus libero incidunt recusandae tenetur vel deserunt, non
-            eveniet, fuga alias culpa!
-          </p>
-        </Section.Wrapper>
-      </Section>
+        <Section className="spikes">
+          <Section.Wrapper>
+            <Section.Title title="section 3" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+              neque velit saepe hic blanditiis maxime ipsam assumenda
+              consectetur, voluptatibus libero incidunt recusandae tenetur vel
+              deserunt, non eveniet, fuga alias culpa!
+            </p>
+          </Section.Wrapper>
+        </Section>
       </HelmetProvider>
     </>
   );
