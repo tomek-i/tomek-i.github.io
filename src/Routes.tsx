4f335ca9874c1c04dab6c490f9951b0b472003e3
tomek-i <tomek.iwainski@gmail.com>
2024-01-11 23:28:02 +1100

    Remove unused code and add contact page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,12 +5,9 @@ import { Config } from './configuration';
 import { Layout } from './layouts/default';
 import { ErrorPage } from './pages/error';
 import { HomePage } from './pages/home';
-import { StatsPage } from './pages/stats';
 import { Frontmatter, Post } from './types';
 
-//TODO: create components instead
 export const Routes = (posts?: Post<Frontmatter>[]) => {
-  const isDev = (process.env.NODE_ENV ?? 'production') !== 'production';
   return (
     <Router>
       <Route path="/" element={<Layout />}>
@@ -26,26 +23,16 @@ export const Routes = (posts?: Post<Frontmatter>[]) => {
             ))}
           </Route>
         )}
+        <Route
+          path="contact"
+          element={
+            <div>
+              TODO: add contact page which will the contact form and some spill
+              and other contact cards liek scial media etc.
+            </div>
+          }
+        />
 
-        {isDev && (
-          <>
-            <Route path="career" element={<div> TODO: add career page </div>} />
-            <Route
-              path={Config().urls?.projectDetails}
-              element={<div> TODO: add projects page </div>}
-            />
-            <Route
-              path="contact"
-              element={
-                <div>
-                  TODO: add contact page which will the contact form and some
-                  spill and other contact cards liek scial media etc.
-                </div>
-              }
-            />
-            <Route path="stats" element={<StatsPage />} />
-          </>
-        )}
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Router>
