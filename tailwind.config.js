7dd8600fd1fdcda63ab3061483c6c8d7617bec23
tomek-i <tomek.iwainski@gmail.com>
2024-01-12 13:02:35 +1100

    Add additional animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,43 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { corinthia: ['Corinthia', ...defaultTheme.fontFamily.sans] },
+      keyframes: {
+        // 'fade-in-down': {
+        //   '0%': {
+        //     opacity: '0',
+        //     transform: 'translateY(-10px)',
+        //   },
+        //   '100%': {
+        //     opacity: '1',
+        //     transform: 'translateY(0)',
+        //   },
+        // },
+        'fade-in': {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        'fade-out': {
+          '0%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in': 'fade-in 0.5s ease-in',
+        'fade-out': 'fade-out 0.5s ease-out',
+      },
     },
   },
   plugins: [],
