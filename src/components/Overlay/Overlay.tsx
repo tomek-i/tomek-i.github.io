6f981feac82ccdfb5889645767767e2879fa7d8d
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 14:46:42 +1000

    chore: TODO: Add blur effect to overlay for background

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,5 +1,6 @@
 export interface OverlayProps extends React.PropsWithChildren {}
 
+// TODO: add a blur effect to the overlay for the background
 export const Overlay: React.FC<OverlayProps> = () => {
   return <div className="fixed inset-0 z-40 bg-black opacity-40"></div>;
 };
