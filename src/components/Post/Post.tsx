66a64947e2016e60de55312c794fd3842e3bb31f
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 14:46:49 +1000

    Update rel attribute for external link in Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -31,7 +31,7 @@ export const Post: React.FC<PostProps> = ({ post }) => {
         <Link
           to={post.frontmatter.company.website}
           target="_blank"
-          rel="nofollow"
+          rel="nofollow external"
         >
           <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200">
             Visit{' '}
