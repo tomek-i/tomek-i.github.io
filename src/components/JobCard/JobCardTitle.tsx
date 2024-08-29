c0cdf9e14e94fdf0e991eb3c53f9711bd685434b
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 15:02:20 +1000

    chore: TODO: Add link to company's website in JobCardTitle component

--- src/components/JobCard/JobCardTitle.tsx
+++ src/components/JobCard/JobCardTitle.tsx
@@ -3,6 +3,8 @@ import { useJobCardContext } from './useCardContext';
 interface JobCardTitleProps {}
 export const JobCardTitle: React.FC<JobCardTitleProps> = () => {
   const { company } = useJobCardContext();
+
+  // TODO: Instead of just displaying the company name, display a link to the company's website
   return (
     <div className="text-left uppercase job-card_title">{company.name}</div>
   );
