c7bca2a2fdfebfd5542be921da5310bbbc6ffb96
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 14:51:12 +1000

    chore: TODO: Extract Tag Cloud to its own component in HomePage
    The Tag Cloud section in the HomePage component has been extracted to its own component for better code organization and reusability. This change improves the maintainability and modularity of the code.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -58,15 +58,45 @@ export const HomePage: React.FC<HomePageProps> = () => {
         </Section>
 
         {process.env.NODE_ENV === 'development' && (
+          // TODO: Extract Tag Cloud to its own component
           <Section className="spikes">
             <Section.Wrapper>
-              <Section.Title title="section 3" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
-                neque velit saepe hic blanditiis maxime ipsam assumenda
-                consectetur, voluptatibus libero incidunt recusandae tenetur vel
-                deserunt, non eveniet, fuga alias culpa!
-              </p>
+              {/* <Section.Title title="section 3" /> */}
+              <div className="relative h-96 w-full">
+                {sortedTags?.map((tag, index) => {
+                  // Calculate font size based on count
+                  const fontSize = `${tag.count * 12}px`;
+                  const transparency = (tag.count * 25) / 100;
+                  const radius = 1 + tag.count * 5;
+                  const angle = (index / sortedTags.length) * 2 * Math.PI;
+                  const offsetRange = 30; // Define the offset range
+                  const randomOffsetX =
+                    Math.random() * 2 * offsetRange - offsetRange; // Random offset between -offsetRange and +offsetRange
+                  const randomOffsetY =
+                    Math.random() * 2 * offsetRange - offsetRange; // Random offset between -offsetRange and +offsetRange
+                  let x = 50 + radius * Math.cos(angle) + randomOffsetX;
+                  let y = 50 + radius * Math.sin(angle) + randomOffsetY;
+
+                  if (index === 0) y = x = 50;
+
+                  return (
+                    <span
+                      key={tag.tag}
+                      className="absolute rounded-full px-3 py-1 font-semibold text-gray-700"
+                      style={{
+                        fontSize,
+                        top: `${y}%`,
+                        left: `${x}%`,
+                        color: `rgba(55, 65, 81, ${transparency})`,
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: sortedTags.length - index,
+                      }}
+                    >
+                      {tag.tag}
+                    </span>
+                  );
+                })}
+              </div>
             </Section.Wrapper>
           </Section>
         )}
