bdb3a72164359a2e8c5cbb364e2d7f9d266b103d
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 14:46:28 +1000

    chore: Refactor usePosts hook to improve code organization and add tag count functionality
    The usePosts hook in the src/components/hooks/usePosts.ts file has been refactored to improve code organization and readability. Additionally, the hook now includes functionality to count the number of tags associated with each post. This change enhances the overall functionality and maintainability of the code.

--- src/components/hooks/usePosts.ts
+++ src/components/hooks/usePosts.ts
@@ -1,11 +1,21 @@
 import fm from 'front-matter';
 import { useEffect, useState } from 'react';
 
-import { desc, parseDateInAustralianFormat } from '../../utility';
+import {
+  countTags,
+  desc,
+  parseDateInAustralianFormat,
+  TagCount,
+} from '../../utility';
 import { Frontmatter, Post as PostType } from './../../types';
+
+type Module = {
+  default: string;
+};
 
 export const usePosts = () => {
   const [posts, setPosts] = useState<PostType<Frontmatter>[]>();
+  const [tags, setTags] = useState<TagCount[]>();
   const [isLoading, setIsLoading] = useState(false);
 
   const importAll = (r: any) => r.keys().map(r);
@@ -17,52 +27,55 @@ export const usePosts = () => {
     (require as any).context(process.env.REACT_APP_CONTENT_PATH, false, /\.md$/)
   );
 
+  const fetchMarkdownFile = async (
+    file: Module
+  ): Promise<PostType<Frontmatter>> => {
+    const response = await fetch(file.default);
+    const content = await response.text();
+    const frontmatter = fm(content).attributes as Frontmatter;
+
+    if (typeof frontmatter.job.dates.start === 'string') {
+      frontmatter.job.dates.start = parseDateInAustralianFormat(
+        frontmatter.job.dates.start
+      );
+    }
+
+    if (typeof frontmatter.job.dates.end === 'string') {
+      frontmatter.job.dates.end = parseDateInAustralianFormat(
+        frontmatter.job.dates.end
+      );
+    }
+
+    return {
+      content,
+      frontmatter,
+    } as PostType<Frontmatter>;
+  };
+
   useEffect(() => {
     const loadContent = async () => {
       setIsLoading(true);
+
       let results = await Promise.all<PostType<Frontmatter>[]>(
-        markdownFiles.map(async (file: any) => {
-          return await fetch(file.default)
-            .then((res) => res.text())
-            .then((content) => {
-              const frontmatter = fm(content).attributes as Frontmatter;
-
-              // Create an object with the desired format options
-
-              if (typeof frontmatter.job.dates.start === 'string') {
-                frontmatter.job.dates.start = parseDateInAustralianFormat(
-                  frontmatter.job.dates.start
-                );
-              }
-
-              // Parse the end date if it's a string
-              if (typeof frontmatter.job.dates.end === 'string') {
-                frontmatter.job.dates.end = parseDateInAustralianFormat(
-                  frontmatter.job.dates.end
-                );
-              }
-              return {
-                content,
-                frontmatter,
-              } as PostType<Frontmatter>;
-            });
-        })
-      ).then((results) =>
-        results.sort((a, b) => {
-          return desc(
-            a.frontmatter.job.dates.start,
-            b.frontmatter.job.dates.start
-          );
-        })
+        markdownFiles.map(fetchMarkdownFile)
       );
+
+      setTags(countTags(results));
+      results.sort((a, b) => {
+        return desc(
+          a.frontmatter.job.dates.start,
+          b.frontmatter.job.dates.start
+        );
+      });
 
       setIsLoading(false);
       setPosts(results);
     };
+
     loadContent();
-    // NOTE: adding the dependecny here will kill the process, probably need to useRef ?
+    // NOTE: adding the dependency here will kill the process, probably need to useRef ?
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { posts, isLoading };
+  return { posts, tags, isLoading };
 };
