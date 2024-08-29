533d12f273c652391d5e84f9d98d9a7c91fb40d3
tomek-i <tomek.iwainski@gmail.com>
2024-08-29 10:56:36 +1000

    chore: Update link text for job details button in TimelineItem component

--- src/components/TimelineItem/TimelineItem.tsx
+++ src/components/TimelineItem/TimelineItem.tsx
@@ -63,10 +63,10 @@ export function TimelineItem({ timeline, isAlternate }: TimelineItemProps) {
               <Link
                 to={`career/${timeline.company.name}`}
                 className={
-                  'px-4 py-2 border rounded my-4 block text-center hover:bg-white hover:text-black'
+                  'px-4 py-2 border rounded my-4 block text-center bg-gray-400 hover:bg-white hover:text-black'
                 }
               >
-                Details
+                View Details
               </Link>
             </JobCard.Info>
           }
